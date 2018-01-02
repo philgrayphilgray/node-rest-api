8a096345379b0d294bf5a19af5783a61a06d6662
philgrayphilgray <you@example.comclear>
2018-01-02 10:18:50 -0500

    call next after cors handling

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
   }
+  next();
 });
 
 app.use("/products", productRoutes);

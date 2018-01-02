f7b3da8f4fa40da8cf07afe57c6a34793e6502d1
philgrayphilgray <you@example.comclear>
2018-01-02 12:33:12 -0500

    complete video 6

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products"
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      // if(docs.length >= 0){
+      res.status(200).json(docs);
+
+      // }
+      // else{
+      //   res.status(404).json({
+      //     message: 'No entries found'
+      //   })
+      // }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err, message: "Nothing there" });
+    });
 });
 router.post("/", (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  };
+  });
+
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
   res.status(200).json({
     message: "Handling POST requests to /products",
     createdProduct: product
@@ -18,26 +44,56 @@ router.post("/", (req, res, next) => {
 });
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "You discovered the special ID",
-      id: id
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "You passed " + id
-    });
-  }
 });
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product!"
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+        message: "something went wrong"
+      });
+    });
 });
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted product!"
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({});
+    })
+    .catch(err => {
+      res.status(500).json({ error: err, message: "Nothing to delete" });
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+const db_url =
+  "mongodb://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@node-rest-shop-shard-00-00-3lnby.mongodb.net:27017,node-rest-shop-shard-00-01-3lnby.mongodb.net:27017,node-rest-shop-shard-00-02-3lnby.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin";
+
+mongoose.connect(db_url, {
+  useMongoClient: true
+});
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));

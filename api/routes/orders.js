b66f230f93ab95b10ddf9501a709cbe603ee41d2
philgrayphilgray <you@example.comclear>
2018-01-02 10:12:51 -0500

    add nodemon and middleware, morgan, and bodyParser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,8 +7,25 @@ router.get("/", (req, res, next) => {
   });
 });
 
+router.post("/", (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  };
+  res.status(201).json({
+    message: "Order was created",
+    order: order
+  });
+});
+
 router.post("/:orderId", (req, res, next) => {
   res.status(201).json({
+    message: "Order dtails",
+    orderId: req.params.orderId
+  });
+});
+router.get("/:orderId", (req, res, next) => {
+  res.status(200).json({
     message: "Order dtails",
     orderId: req.params.orderId
   });
